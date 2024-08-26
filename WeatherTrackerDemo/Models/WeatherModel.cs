b3370ca61910bc454f3fc10fc700904c2b39b1fd
Tyler Joy <tyler.david.joy@eagles.oc.edu>
2024-08-26 00:49:09 -0500

    Changed weather api to one that is more accurate Updated UI to better show information

--- WeatherTrackerDemo/Models/WeatherModel.cs
+++ WeatherTrackerDemo/Models/WeatherModel.cs
@@ -6,49 +6,93 @@ namespace WeatherTrackerDemo.Models
     public class WeatherModel
     {
         public string City { get; set; }
-        public WeatherItem Current { get; set; }
-        public List<WeatherItem> Forecast { get; set; }
+        public List<WeatherItem> TwelveHourForecast { get; set; }
+        public List<WeatherItem> FiveDayForecast { get; set; }
 
         public async Task GetWeather(string apikey)
         {
+            TwelveHourForecast = new List<WeatherItem>();
+            FiveDayForecast = new List<WeatherItem>();
+
             using (HttpClient client = new HttpClient())
             {
-                var url = new Uri($"https://api.weatherapi.com/v1/forecast.json?q={City}&days=3&key={apikey}");
+                var locationkey = 350143;
+                //12-Hour Forecast
+                var url = new Uri($"http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/{locationkey}?apikey={apikey}&details=true");
                 var res = await client.GetAsync(url);
 
                 if (res.IsSuccessStatusCode)
                 {
                     var resString = await res.Content.ReadAsStringAsync();
-                    var json = (JObject)JsonConvert.DeserializeObject(resString);
-                    this.Current = new WeatherItem(
-                        json["current"]["condition"]["icon"].Value<string>(),
-                        json["current"]["temp_f"].Value<string>(),
-                        json["current"]["feelslike_f"].Value<string>(),
-                        0,
-                        json["current"]["humidity"].Value<int>(),
-                        json["current"]["wind_mph"].Value<string>() + " mph " + json["current"]["wind_dir"].Value<string>(),
-                        json["current"]["uv"].Value<int>(),
-                        json["current"]["pressure_in"].Value<float>(), "", "",
-                        json["current"]["last_updated"].Value<string>());
+                    var json = (JArray)JsonConvert.DeserializeObject(resString);
 
-                    this.Forecast = new List<WeatherItem>();
-                    var temp = json["forecast"]["forecastday"][0];
-                    for (var i = 0; i < 3; i++)
+                    foreach (var hour in json)
                     {
-                        Forecast.Add(new WeatherItem(
-                        json["forecast"]["forecastday"][i]["day"]["condition"]["icon"].Value<string>(),
-                        "",
-                        "",
-                        Math.Max(json["forecast"]["forecastday"][i]["day"]["daily_chance_of_rain"].Value<int>(), json["forecast"]["forecastday"][i]["day"]["daily_chance_of_snow"].Value<int>()),
-                        json["forecast"]["forecastday"][i]["day"]["avghumidity"].Value<int>(),
-                        json["forecast"]["forecastday"][i]["day"]["maxwind_mph"].Value<string>() + " mph " + json["forecast"]["forecastday"][i]["hour"][0]["wind_dir"].Value<string>(),
-                        json["forecast"]["forecastday"][i]["day"]["uv"].Value<int>(),
-                        0,
-                        json["forecast"]["forecastday"][i]["day"]["mintemp_f"].Value<string>(),
-                        json["forecast"]["forecastday"][i]["day"]["maxtemp_f"].Value<string>(),
-                        json["forecast"]["forecastday"][i]["date"].Value<string>()));
+                        TwelveHourForecast.Add(new WeatherItem(
+                            hour["WeatherIcon"].Value<string>(),
+                            "TEST",
+                            hour["Temperature"]["Value"].Value<float>(),
+                            hour["RealFeelTemperature"]["Value"].Value<float>(),
+                            hour["PrecipitationProbability"].Value<int>(),
+                            hour["RelativeHumidity"].Value<int>(),
+                            hour["Wind"]["Speed"]["Value"].Value<string>() + " MPH " + hour["Wind"]["Direction"]["Localized"].Value<string>(),
+                            hour["UVIndexText"].Value<string>(),
+                            0,
+                            0,
+                            hour["DateTime"].Value<DateTime>()
+                            ));
+                    }
+
+                    //5 Day Forecast
+                    url = new Uri($"http://dataservice.accuweather.com/forecasts/v1/daily/5day/{locationkey}?apikey={apikey}&details=true");
+                    res = await client.GetAsync(url);
+
+                    if(res.IsSuccessStatusCode)
+                    {
+                        resString = await res.Content.ReadAsStringAsync();
+                        var json1 = (JObject)JsonConvert.DeserializeObject(resString);
+
+                        foreach(var day in json1["DailyForecasts"])
+                        {
+                            FiveDayForecast.Add(new WeatherItem(
+                                day["Day"]["Icon"].Value<string>(),
+                                "TEST",
+                                0,
+                                0,
+                                Math.Max(day["Day"]["PrecipitationProbability"].Value<int>(), day["Night"]["PrecipitationProbability"].Value<int>()),
+                                day["Day"]["RelativeHumidity"]["Maximum"].Value<int>(),
+                                day["Day"]["Wind"]["Speed"]["Value"].Value<string>() + " MPH " + day["Day"]["Wind"]["Direction"]["Localized"].Value<string>(),
+                                day["AirAndPollen"][5]["Category"].Value<string>(),
+                                day["Temperature"]["Minimum"]["Value"].Value<float>(),
+                                day["Temperature"]["Maximum"]["Value"].Value<float>(),
+                                day["Date"].Value<DateTime>()));
+                        }
                     }
                 }
+                else
+                {
+                    GetWeatherTest();
+                }
+            }
+        }
+
+        public async Task GetWeatherTest()
+        {
+            TwelveHourForecast = new List<WeatherItem>();
+            FiveDayForecast = new List<WeatherItem>();
+
+            var time = DateTime.Now;
+            for(var i = 0; i < 12; i++)
+            {
+                TwelveHourForecast.Add(new WeatherItem("1", "Sunny", 75, 80, 10, 70, "20 mph SSE", "High", 60f, 85f, time));
+                time = time.AddHours(1);
+            }
+
+            time = DateTime.Now;
+            for (var i = 0; i < 5; i++)
+            {
+                FiveDayForecast.Add(new WeatherItem("1", "Sunny", 0, 0, 40, 70, "20 mph SSE", "High", 60f, 85f, time));
+                time = time.AddDays(1);
             }
         }
     }
@@ -56,29 +100,29 @@ namespace WeatherTrackerDemo.Models
     public class WeatherItem
     {
         public string Img { get; set; }
-        public string Temperature { get; set; } //Temp - C / F
-        public string FeelsLike { get; set; } //Temp - C / F
-        public string High {  get; set; }
-        public string Low { get; set; }
+        public string Description { get; set; } //Weather type
+        public float Temperature { get; set; } //Temp - C / F
+        public float FeelsLike { get; set; } //Temp - C / F
+        public float High {  get; set; }
+        public float Low { get; set; }
         public int PrecipitationChance { get; set; }
         public int Humidity { get; set; }
         public string Wind { get; set; } //Direction - Speed
-        public int UV { get; set; }
-        public float Pressure { get; set; }
-        public string DayOfWeek { get; set; }
-        public WeatherItem(string img, string temperature, string feelsLike, int precipitationChance, int humidity, string wind, int uV, float pressure, string low, string high, string date)
+        public string UV { get; set; }
+        public DateTime Timestamp { get; set; }
+        public WeatherItem(string img, string description, float temperature, float feelsLike, int precipitationChance, int humidity, string wind, string uV, float low, float high, DateTime date)
         {
             Img = img;
-            Temperature = temperature + "°F";
-            FeelsLike = feelsLike + "°F";
+            Description = description;
+            Temperature = temperature;
+            FeelsLike = feelsLike;
             PrecipitationChance = precipitationChance;
             Humidity = humidity;
             Wind = wind;
             UV = uV;
-            Pressure = pressure;
-            Low = low + "°F";
-            High = high + "°F";
-            DayOfWeek = DateTime.Parse(date).DayOfWeek.ToString();
+            Low = low;
+            High = high;
+            Timestamp = date;
         }
     }
 }
